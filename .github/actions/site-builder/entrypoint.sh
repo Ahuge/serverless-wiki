0db3c040c0cda126949917c16aae7681bb1cb38e
Alex <alex@vitalityvfx.com>
2020-06-12 20:45:27 -0700

    Update our entrypoint

--- .github/actions/site-builder/entrypoint.sh
+++ .github/actions/site-builder/entrypoint.sh
@@ -10,14 +10,14 @@ export CODE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 [[ -d source ]] || exit "Please set SOURCE to the source git directory"
 [[ -d target ]] || exit "Please set TARGET to the target git directory"
 
-cp -r ${CODE}/resources/* target
+cp -r ${CODE}/website/resources/* target
 cp -r source/users target
 
 # And convert $SOURCE/pages/*.md to $TARGET/*.html :)
 for file in `cd source; find . -name "*.md"`; do
   mkdir -p target/`dirname ${file}`
   echo "Processing source/$file into target/${file%md}html"
-  python ${CODE}/convert_page.py < source/${file} > target/${file%md}html
+  python ./convert_page.py < source/${file} > target/${file%md}html
 done
 # end generate.sh
 
