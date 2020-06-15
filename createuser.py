77eec939e7f18662d7df7784287d19ecfea1c84e
Alex <alex@vitalityvfx.com>
2020-06-15 09:26:33 -0700

    Format for hocon ouutput

--- createuser.py
+++ createuser.py
@@ -9,7 +9,7 @@ fullname = sys.argv[2]
 password = sys.argv[3]
 iterations = 10
 
-nonce = os.environ['NONCE']
+nonce = os.environ['NONCE'].encode("utf-8")
 
 def light_hash(pwd):
   r = 0
@@ -18,12 +18,13 @@ def light_hash(pwd):
   return r
 
 def hash(pwd):
+  pwd = pwd.encode("utf-8")
   salt = bcrypt.gensalt(10)
   first_hash = bcrypt.hashpw(pwd, salt)
-  return bcrypt.hashpw(nonce + first_hash, salt)
+  return bcrypt.hashpw(nonce + first_hash, salt).decode("utf-8")
 
 print('{')
-print('  full_name: "' + fullname + '"')
-print('  password_light_hash: %d' % light_hash(password))
-print('  password_hash: "' + hash(password) + '"')
+print('  full_name= "' + fullname + '"')
+print('  password_light_hash= %d' % light_hash(password))
+print('  password_hash= "' + hash(password) + '"')
 print('}')
